143c7d22dc7794ab1c2ead84feacd43f8b9a0d67
Всеволод <vsevolodbznk.it@gmail.com>
2022-11-09 23:58:11 +0200

    index.tsx update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/testfelaab">
     <App />
   </BrowserRouter>
 );
